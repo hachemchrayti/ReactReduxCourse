fbb5cfe666d6a7ca00721ae4f3f7db1acec37d51
hachemchrayti <hachem.chrayti@outlook.fr>
2021-11-25 19:27:59 +0100

    using conditional ouput

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react'
 
 
-const Ninjas = (props) =>
+//const Ninjas = (props) =>
+const Ninjas = ({ ninjas }) =>
 {
-    const { ninjas } = props
+    let color = '';
     const ninjasList = ninjas.map(ninja =>
     {
-        return (
-            <div className='ninja' key={ninja.id}>
-                <div>Name: {ninja.name}</div>
-                <div>age: {ninja.age}</div>
-                <div>belt: {ninja.belt}</div>
-                <p>**********************</p>
-            </div>
-        )
+        if (ninja.age > 30)
+        {
+            return (
+                <div className='ninja' key={ninja.id}>
+                    {color = ninja.name.startsWith('h') ? 'green' : 'red'}
+                    <div style={{ color: color }}>Name: {ninja.name}</div>
+                    <div>age: {ninja.age}</div>
+                    <div>belt: {ninja.belt}</div>
+                    <p>**********************</p>
+                </div>
+            )
+        } else
+        {
+            return null
+        }
+
     })
     return (
         <div>
