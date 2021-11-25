74ea40b9c084f7d9c077794f8935e7ecf048e8dd
hachemchrayti <hachem.chrayti@outlook.fr>
2021-11-25 19:19:15 +0100

    Transform Ninjas class component to a funcitonal component

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react'
 
-export default class Ninjas extends Component
+
+const Ninjas = (props) =>
 {
-    render()
+    const { ninjas } = props
+    const ninjasList = ninjas.map(ninja =>
     {
-        const { ninjas } = this.props
-        const ninjasList = ninjas.map(ninja =>
-        {
-            return (
-                <div className='ninja' key={ninja.id}>
-                    <div>Name: {ninja.name}</div>
-                    <div>age: {ninja.age}</div>
-                    <div>belt: {ninja.belt}</div>
-                    <p>**********************</p>
-                </div>
-            )
-        })
         return (
-            <div>
-                {ninjasList}
-            </div >
+            <div className='ninja' key={ninja.id}>
+                <div>Name: {ninja.name}</div>
+                <div>age: {ninja.age}</div>
+                <div>belt: {ninja.belt}</div>
+                <p>**********************</p>
+            </div>
         )
-    }
+    })
+    return (
+        <div>
+            {ninjasList}
+        </div>
+    )
 }
+export default Ninjas;
+
+// export default class Ninjas extends Component
+// {
+//     render()
+//     {
+//         const { ninjas } = this.props
+//         const ninjasList = ninjas.map(ninja =>
+//         {
+//             return (
+//                 <div className='ninja' key={ninja.id}>
+//                     <div>Name: {ninja.name}</div>
+//                     <div>age: {ninja.age}</div>
+//                     <div>belt: {ninja.belt}</div>
+//                     <p>**********************</p>
+//                 </div>
+//             ) 
+//         })
+//         return (
+//             <div>
+//                 {ninjasList}
+//             </div>
+//         )
+//     }
+// }
