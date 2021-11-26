be7bf5797ca0b39be6a004da2f9fea367a92c2c9
hachemchrayti <hachem.chrayti@outlook.fr>
2021-11-26 14:13:59 +0100

    REACT ROUTING -  add Navbar component and use React-Router-dom

---
+++ poketimes/src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = (props) =>
+{
+    return (
+        <div className='container'>
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis tempore dolor animi nesciunt repellendus obcaecati, iste doloribus delectus sit fugiat possimus architecto amet quas maiores natus beatae iure! Fugiat, voluptas?</p>
+        </div>
+    )
+}
